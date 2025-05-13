6a910152407d4e47a18dca59f39253aee80b9a0e
Yann B. <ybycode@gmail.com>
2025-05-13 20:53:21 +0200

    Adds mdx and sitemap integrations, .gitignore items, tsconfig.json, removes .astro from git

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 import { defineConfig } from 'astro/config'
 
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://ybycode.github.io',
+  integrations: [mdx(), sitemap()],
 })
